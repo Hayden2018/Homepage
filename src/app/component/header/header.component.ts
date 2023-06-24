7464ee97020117ac7c2125db8ff85cd29b58460c
Hayden2018 <yikhei123@gmail.com>
2023-06-24 22:54:03 +0800

    Fix header routing issue

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core'
 import { Store } from '@ngrx/store';
 import { toggleMobileMenu } from 'src/app/store/store.action';
 import { bindState } from 'src/app/utils';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -13,29 +15,25 @@ export class HeaderComponent implements OnInit {
   isMobile: boolean = window.innerWidth <= 768;
   bind = bindState;
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private router: Router) {
     this.bind(store, (state: any) => state.screen.isMobile, 'isMobile');
   }
 
   state = ['inactive', 'inactive'];
 
   ngOnInit(): void {
-    if (window.location.pathname.includes('/profile')) {
-      this.state[0] = 'active';
-    }
-    else if (window.location.pathname.includes('/work')) {
-      this.state[1] = 'active';
-    }
-    else {
-      this.state[0] = 'active';
-    }
-  }
-
-  selectTab = (index: number): void => {
-    this.state = ['inactive', 'inactive'];
-    if (this.state[index] !== 'active') {
-      this.state[index] = 'active';
-    }
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+      if (event.url.includes('work')) {
+        this.state[0] = 'inactive';
+        this.state[1] = 'active';
+      }
+      else {
+        this.state[1] = 'inactive';
+        this.state[0] = 'active';
+      }
+    });
   }
 
   toggleMenu = (): void => {
